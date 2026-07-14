b4d90e0cdb9457bb2dab5ae760e8c4fcb3a1678e
Devanshu Gupta <devanshugupta1896@gmail.com>
2026-07-14 10:08:29 +0530

    Generate image embeddings using OpenCLIP

--- indexer/generate_embeddings.py
+++ indexer/generate_embeddings.py
@@ -1,6 +1,4 @@
 from pathlib import Path
-import json
-import random
 from tqdm import tqdm
 
 from models.clip_model import CLIPModel
@@ -11,28 +9,22 @@ class EmbeddingGenerator:
     def __init__(self):
         self.clip = CLIPModel()
 
-    def generate(self, image_dir, output_file):
+    def generate(self):
 
-        image_dir = Path(image_dir)
+        with open("data/selected_images.txt") as f:
+            image_paths = [
+                Path(line.strip())
+                for line in f
+            ]
 
-        image_paths = sorted(image_dir.glob("*"))
-
-        random.seed(42)
-        random.shuffle(image_paths)
-        image_paths = image_paths[:800]
-
-        embeddings = []
+        embeddings = {}
 
         for image_path in tqdm(image_paths):
 
-            embedding = self.clip.encode_image(str(image_path))
+            embeddings[str(image_path)] = (
+                self.clip.encode_image(
+                    str(image_path)
+                ).tolist()
+            )
 
-            embeddings.append({
-                "image_path": str(image_path),
-                "embedding": embedding.tolist()
-            })
-
-            with open(output_file, "w") as f:
-                json.dump(embeddings, f)
-
-        print(f"Saved {len(embeddings)} embeddings.")
+        return embeddings
